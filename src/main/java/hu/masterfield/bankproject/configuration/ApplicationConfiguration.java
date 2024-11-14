dbe241e932b24c23c7103e60fdff184358b01d89
ddave5 <felegyhazi.david@gmail.com>
2024-11-14 11:18:27 +0100

    last part of task 6

--- src/main/java/hu/masterfield/bankproject/configuration/ApplicationConfiguration.java
+++ src/main/java/hu/masterfield/bankproject/configuration/ApplicationConfiguration.java
@@ -1,17 +1,8 @@
 package hu.masterfield.bankproject.configuration;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import hu.masterfield.bankproject.interfaces.AccountRepository;
-import hu.masterfield.bankproject.interfaces.TransferService;
-import hu.masterfield.bankproject.services.JdbcAccountRepository;
-import hu.masterfield.bankproject.services.TransferServiceImpl;
 
 
 @Configuration
@@ -19,18 +10,6 @@ import hu.masterfield.bankproject.services.TransferServiceImpl;
 @Import(InfraConfiguration.class)
 public class ApplicationConfiguration {
 
-    @Autowired
-    DataSource ds;
-    
-    @Bean
-    public TransferService transferService() {
-        return new TransferServiceImpl(accountRepository());
-    }
-
-    @Bean
-    public AccountRepository accountRepository() {
-        return new JdbcAccountRepository(ds);
-    }
 
     
 }
