82c1cad2c14d5eac6ab61e853d23c13178c703b9
ddave5 <felegyhazi.david@gmail.com>
2024-11-14 15:57:36 +0100

    task11

--- src/test/java/hu/masterfield/bankproject/TransferServiceTests.java
+++ src/test/java/hu/masterfield/bankproject/TransferServiceTests.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
@@ -18,20 +20,27 @@ import hu.masterfield.bankproject.interfaces.TransferService;
 
 @SpringBootTest
 @ContextConfiguration(classes=XMLApplicationConfiguration.class)
+@TestPropertySource("/test_data.properties")
 public class TransferServiceTests {
+
+    private final int START_BALANCE = 10000;
 
     @Autowired
     private TransferService service;
 
     @Test
     @Sql(scripts = "initbalance.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
-    public void transfer() {
+    public void transfer(
+        @Value("${test.fromAccountName}") String fromAccountName,
+        @Value("${test.toAccountName}") String toAccountName,
+        @Value("${test.amount}") int amount
+        ) {
         Confirmation receipt;
 
-        receipt = service.transfer("Szegény Szilárd", "Pénzes Péter", 1000);
+        receipt = service.transfer(fromAccountName, toAccountName, amount);
 
         System.out.println(receipt.getMessage());
 
-        assertEquals(9000, receipt.getNewBalance());
+        assertEquals(START_BALANCE - amount, receipt.getNewBalance());
     }
 }
