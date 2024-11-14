6b6a4e3c5d64235ded253d4c6935c7e471c9c359
ddave5 <felegyhazi.david@gmail.com>
2024-11-14 15:42:42 +0100

    task10

--- src/test/java/hu/masterfield/bankproject/TransferServiceTests.java
+++ src/test/java/hu/masterfield/bankproject/TransferServiceTests.java
@@ -4,24 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
-import hu.masterfield.bankproject.configuration.ApplicationConfiguration;
+import hu.masterfield.bankproject.configuration.XMLApplicationConfiguration;
 import hu.masterfield.bankproject.datatypes.Confirmation;
 import hu.masterfield.bankproject.interfaces.TransferService;
 
+@SpringBootTest
+@ContextConfiguration(classes=XMLApplicationConfiguration.class)
 public class TransferServiceTests {
+
+    @Autowired
     private TransferService service;
 
-    @BeforeEach
-    public void setUp() {
-        ApplicationContext context = SpringApplication.run(ApplicationConfiguration.class);
-
-        service = (TransferService) context.getBean("transferService");
-    }
-
     @Test
+    @Sql(scripts = "initbalance.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
     public void transfer() {
         Confirmation receipt;
 
