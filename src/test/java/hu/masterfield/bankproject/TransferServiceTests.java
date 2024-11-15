1b19191daf598acc2b867d2e107d4d198ce0c1f4
ddave5 <felegyhazi.david@gmail.com>
2024-11-15 11:43:56 +0100

    task12 tests

--- src/test/java/hu/masterfield/bankproject/TransferServiceTests.java
+++ src/test/java/hu/masterfield/bankproject/TransferServiceTests.java
@@ -10,10 +10,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.jdbc.JdbcTestUtils;
 
 import hu.masterfield.bankproject.configuration.XMLApplicationConfiguration;
 import hu.masterfield.bankproject.datatypes.Confirmation;
 import hu.masterfield.bankproject.interfaces.TransferService;
+import hu.masterfield.bankproject.services.JdbcAccountRepository;
 
 @SpringBootTest
 @ContextConfiguration(classes=XMLApplicationConfiguration.class)
@@ -24,6 +26,9 @@ public class TransferServiceTests {
 
     @Autowired
     private TransferService service;
+
+    @Autowired
+    private JdbcAccountRepository jdbcRep;
 
     @Test
     @Sql(scripts = "initbalance.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
@@ -39,5 +44,7 @@ public class TransferServiceTests {
         System.out.println(receipt.getMessage());
 
         assertEquals(START_BALANCE - amount, receipt.getNewBalance());
+
+        assertEquals(4, JdbcTestUtils.countRowsInTable(jdbcRep.getJdbcTemplate(), "ACCOUNTS"));
     }
 }
